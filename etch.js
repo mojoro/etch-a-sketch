a6f03db888ee4ce0f72b3db3cafafd19a5a8ca28
mojoro <45500240+mojoro@users.noreply.github.com>
2024-03-07 18:30:06 +0100

    Begin logic for new button to draw new grid

--- etch.js
+++ etch.js
@@ -1,7 +1,22 @@
-for (let i = 0; i < 16*16; i++) {
-    const gridSquare = document.createElement('div');
-    gridSquare.className = 'grid-square';
+const newGridBtn = document.querySelector('button');
 
-    const centering = document.querySelector('.centering');
-    centering.appendChild(gridSquare);
+newGridBtn.addEventListener('click', () => {
+    const gridSize = parseInt(prompt("How many boxes to a side do you want for your grid?"));
+    drawGrid(gridSize);
+});
+
+
+function drawGrid(gridSize) {
+    for (let i = 0; i < gridSize*gridSize; i++) {
+        const gridSquare = document.createElement('div');
+        gridSquare.className = 'grid-square';
+
+        const centering = document.querySelector('.centering');
+        centering.appendChild(gridSquare);
+        gridSquare.addEventListener('pointerover', () => {
+            gridSquare.style.backgroundColor = 'black'
+        });
+    }
 }
+
+drawGrid(16);
