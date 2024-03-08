1349308d92062be6c0bcc45b7072bedd561379bf
mojoro <45500240+mojoro@users.noreply.github.com>
2024-03-08 13:13:24 +0100

    Finish logic for new grid and clearing old grid

--- etch.js
+++ etch.js
@@ -1,22 +1,34 @@
 const newGridBtn = document.querySelector('button');
 
 newGridBtn.addEventListener('click', () => {
-    const gridSize = parseInt(prompt("How many boxes to a side do you want for your grid?"));
-    drawGrid(gridSize);
+    const gridSize = parseInt(prompt("How many boxes to a side do you want for your grid? (max 100)"));
+    clearGrid();
+    if (gridSize > 100) alert("Too large! Try again.");
+    else drawGrid(gridSize);
 });
 
-
+// I think I need to adjust the size of the grid squares based on the number of squares vs the size of the grid.
 function drawGrid(gridSize) {
     for (let i = 0; i < gridSize*gridSize; i++) {
         const gridSquare = document.createElement('div');
         gridSquare.className = 'grid-square';
-
+        gridSquare.style.width = `${900/gridSize}px`;
+        gridSquare.style.height = `${900/gridSize}px`;
         const centering = document.querySelector('.centering');
         centering.appendChild(gridSquare);
         gridSquare.addEventListener('pointerover', () => {
             gridSquare.style.backgroundColor = 'black'
         });
     }
+}
+
+function clearGrid(){
+    const centering = document.querySelector(".centering");
+
+    while(centering.firstChild){
+        centering.removeChild(centering.firstChild);
+    }
+    
 }
 
 drawGrid(16);
--- instructions.md
+++ instructions.md
@@ -17,6 +17,8 @@ Don’t forget to commit early & often! You can [reference the Commit Message l
     - There are multiple ways to change the color of the divs, including:
         - Adding a new class to the div.
         - Changing the div’s background color using JavaScript.
+
+## Not easy - left off here. Need to make the grid boxes able to shrink to fit to a grid. Need to figure out how to specify an amount of boxes per grid.
 4. Add a button to the top of the screen that will send the user a popup asking for the number of squares per side for the new grid. Once entered, the existing grid should be removed and a new grid should be generated _in the same total space as before_ (e.g. 960px wide) so that you’ve got a new sketch pad. **Tip**: Set the limit for the user input to a maximum of 100. A larger number of squares results in more computer resources being used, potentially causing delays, freezing, or crashing that we want to prevent.
     - Research `button` tags in HTML and how you can make a JavaScript function run when one is clicked.
     - Also check out `prompt`s.
